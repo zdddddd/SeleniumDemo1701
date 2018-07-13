aa9b3324a862ef20da7f217d639d519e7755f43b
zdddddd <470015321@qq.com>
2018-07-13 16:38:16 +0800

    tianjia

--- src/test/java/com/meyoung/day3/TestGrid.java
+++ src/test/java/com/meyoung/day3/TestGrid.java
@@ -3,6 +3,7 @@ package com.meyoung.day3;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -20,6 +21,20 @@ public class TestGrid {
         Thread.sleep(2000);
         driver.quit();
     }
+
+    @Test
+    public void testFail() throws Exception{
+        //实例化DesiredCapabilities 类
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        //实例化一个driver
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.28:4444/wd/hub"),dc);
+        driver.get("http://www.baidu.com");
+        Thread.sleep(2000);
+        Assert.assertTrue(false);
+        driver.quit();
+    }
+
+
 
     @DataProvider(name="data1")
     public Object [][] test1(){
